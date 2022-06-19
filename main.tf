170d1967809ea73208349830c96ec4288d56f6d7
Vincent Gagnon <vincent.gagnon@gmail.com>
2022-06-19 12:43:09 -0400

    removing tf cloud

--- main.tf
+++ main.tf
@@ -13,12 +13,6 @@ terraform {
       version = ">= 2.1.0"
     }
   }
-    cloud {
-    organization = "Nokos"
-    workspaces {
-      name = "terraform-azure"
-    }
-  }
   required_version = ">= 1.1.0"
 }
 
